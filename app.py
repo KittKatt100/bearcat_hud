15d5871f6d9059be578f88e609c5eb1c1c75e890
FunkDiva <funkulture1@gmail.com>
2025-07-14 05:36:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 import streamlit as st
-from interface import dashboard, team_entry
-
-PAGES = {
-    "Team Entry": team_entry,
-    "Game Dashboard": dashboard
-}
+from interface import dashboard
 
 def main():
-    st.set_page_config(page_title="Bearcat HUD", page_icon="🏈", layout="wide")
-
-    st.sidebar.title("Select Options")
-    default_index = list(PAGES.keys()).index("Team Entry")  # Make Team Entry default
-    page = st.sidebar.radio("Navigate", list(PAGES.keys()), index=default_index)
-
-    PAGES[page].main()
+    dashboard.main()
 
 if __name__ == "__main__":
     main()
