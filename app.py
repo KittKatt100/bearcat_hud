a46c2e06421ccc7949e6eaa3d25f55ab540a7374
FunkDiva <funkulture1@gmail.com>
2025-08-13 01:10:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # app.py
-import streamlit as st
-from interface.dashboard import main as dashboard_main
+from bearcat_hud.interface.dashboard import main
 
-st.set_page_config(page_title="Bearcat HUD", layout="centered")
-dashboard_main()
+if __name__ == "__main__":
+    main()
